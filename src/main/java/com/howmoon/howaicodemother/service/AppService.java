b7e8d875e0395dbc5de613cb4dd8884b90f75ac8
wanghaoran <wanghaoran@ejoy.tech>
2025-08-01 23:54:38 +0800

    应用模块（应用生成）

--- src/main/java/com/howmoon/howaicodemother/service/AppService.java
+++ src/main/java/com/howmoon/howaicodemother/service/AppService.java
@@ -1,7 +1,14 @@
 package com.howmoon.howaicodemother.service;
 
+import com.howmoon.howaicodemother.model.dto.app.AppQueryRequest;
+import com.howmoon.howaicodemother.model.entity.User;
+import com.howmoon.howaicodemother.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.howmoon.howaicodemother.model.entity.App;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -10,4 +17,33 @@ import com.howmoon.howaicodemother.model.entity.App;
  */
 public interface AppService extends IService<App> {
 
+    /**
+     * 获取应用封装类
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 构造应用查询条件
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    /**
+     * 获取应用封装列表
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 通过对话生成应用代码
+     * @param appId 应用ID
+     * @param message 提示词
+     * @param loginUser 登录用户
+     * @return
+     */
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
 }
